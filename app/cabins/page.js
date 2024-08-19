4a6551d08be09b9001c5ce920e2b95bf3a268318
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-08-19 13:18:50 +0600

    Fetch data in a page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,14 @@
-import Navigation from "../components/Navigation";
-
-export default function Page() {
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
   return (
     <div>
-      {/* <Navigation /> */}
       <h1>Cabins Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
