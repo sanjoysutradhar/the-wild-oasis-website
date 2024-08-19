3f3becb728a6d587e01f1a64a2cb3aba998231bd
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-08-19 16:33:07 +0600

    Styling with tailwind css

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@
 
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
 
 // export default function RootLayout({ children }) {
 //   return (
@@ -21,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-900 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
