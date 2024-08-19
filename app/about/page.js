61fc65d9bdec8130ff1ae9942bb1c70dfec46c9a
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-08-19 16:09:49 +0600

    Project Organization

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,6 @@
-import Navigation from "../components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      {/* <Navigation /> */}
       <h1>About of the wild oasis</h1>
     </div>
   );
