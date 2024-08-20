e4ceebe003e30a1b52a0a222c3d43636c15c27bb
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-08-20 11:25:50 +0600

    Building the Home Page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,38 @@
 import Link from "next/link";
 import Navigation from "@/app/_components/Navigation";
 
+import Image from "next/image";
+import bg from "@/public/bg.png";
+
 export default function page() {
   return (
-    <div>
-      {/* <Navigation /> */}
-      <h1>The wild oasis. Welcome to the paradise</h1>
-      {/* <a href="/cabins">Explore luxury cabins</a> */}
-      <Link href="/cabins">Explore luxury cabins</Link>
-    </div>
+    // <div>
+    //   {/* <Navigation /> */}
+    //   <h1>The wild oasis. Welcome to the paradise</h1>
+    //   {/* <a href="/cabins">Explore luxury cabins</a> */}
+    //   <Link href="/cabins">Explore luxury cabins</Link>
+    // </div>
+    <main className="mt-24">
+      <Image
+        src={bg}
+        fill
+        placeholder="blur"
+        quality={100}
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
