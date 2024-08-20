82420e775b1b31cf98651e6cbbb3d056ee534835
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-08-20 18:05:31 +0600

    Streaming UI with Suspense: Cabin List

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -13,17 +13,17 @@
 //     </div>
 //   );
 
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "@/app/_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "@/app/_components/CabinList";
+import Spinner from "@/app/_components/Spinner";
 
 // }
 export const metadata = {
   title: "Cabins",
 };
-export default async function Page() {
+export default function Page() {
   // CHANGE
   // const cabins = [];
-  const cabins = await getCabins();
 
   return (
     <div>
@@ -39,13 +39,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
