824f3548c8119670ddaff803966a37a21ba3d8c0
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-08-29 12:10:18 +0600

    Getting the User Session

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,13 +1,17 @@
 import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import LoginMessage from "./LoginMessage";
+import { auth } from "../_lib/auth";
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
-  console.log(settings);
+  // console.log(settings);
+  const session = await auth();
+
   return (
     <div
       className="grid grid-cols-2 relative
@@ -18,7 +22,11 @@ border border-primary-800 min-h-[400px]"
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
