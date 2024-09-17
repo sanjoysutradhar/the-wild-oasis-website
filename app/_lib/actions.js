fe814b9de8492d2fe0d37223831cebd30b1bbed6
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-09-17 14:52:13 +0600

    Another Loading Indicator:the useTransation Hook

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,9 +3,10 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
-  console.log(formData.get("nationalID"));
+  // console.log(formData.get("nationalID"));
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
   const nationalID = formData.get("nationalID");
@@ -29,6 +30,24 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("You are not allowed to delete this bookings");
+  }
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    throw new Error("Booking could not be deleted");
+  }
+  revalidatePath("/account/reservations");
+}
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
